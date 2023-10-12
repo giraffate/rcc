df32098277d3dc6d3ad1bcb5cf0ad8f089205331
Takayuki Nakata <f.seasons017@gmail.com>
2023-10-12 18:21:54 +0900

    Make Compiler which compile one integer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() != 2 {
+        panic!("invalid arguments!");
+    }
+
+    args.next().unwrap();
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", args.next().unwrap());
+    println!("  ret");
 }
